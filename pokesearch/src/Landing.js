253d5f0d042c3ed72485fbbca4ebcc004b7b31e5
Nathan95 <nathan@mediazoo.tv>
2018-02-06 14:08:15 +0000

    this is a new commit

---
+++ pokesearch/src/Landing.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {View, Text, Button, Image} from 'react-native';
+
+var myBackground = require('../assets/landing.jpg');
+
+class Landing extends React.Component{
+  render() {
+    return(
+        <View>
+          <Image source={myBackground}/>
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to Pokesearch</Text>
+            <Button title='Start Searching'
+              block={true}
+              style={styles.buttonStyle}
+              onPress={()=>this.props.switchScreen("screen")}
+            >
+              <Text style={styles.buttonText}>Start Searching</Text>
+            </Button>
+          </View>
+        </View>
+    )
+  }
+}
+const styles = {
+    viewStyle: {
+      position: 'absolute',
+      top: 300,
+      left: 60,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    titleStyle: {
+      fontSize: 25,
+      color: 'blue'
+    },
+    buttonStyle: {
+      margin: 10
+    },
+    buttonText: {
+      color: 'white'
+    }
+}
+export default Landing;
