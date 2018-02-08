e7e5a45bd1b8f9371d19a1ed51ec1918d6a9ea8e
Nathan95 <nathan@mediazoo.tv>
2018-02-08 12:59:46 +0000

    search functionality front-end done

--- pokesearch/src/Search.js
+++ pokesearch/src/Search.js
@@ -1,20 +1,56 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import { Button, Header, SearchBar } from 'react-native-elements';
+import {View, Text, Image} from 'react-native';
+import { Button, Header, SearchBar, Input } from 'react-native-elements';
+import PokeLoader from './PokeLoader';
+import SearchBody from './SearchBody';
+
+
+var myBackground = require('../assets/tenor.gif');
 
 class Search extends React.Component{
+    state = {
+        pokeSearch: "",
+        onCall: true
+    }
+    searchPoke = () =>{
 
+    }
+    renderBody = () =>{
+      if(this.state.onCall){
+          return(
+              <PokeLoader />
+          )
+      } else {
+          return (
+              <SearchBody />
+          )
+      }
+    }
     render(){
       return(
         <View style={{flex: 1}}>
         <SearchBar
-      
-          placeholder='Type Here...' />
+          round
+          onPress={this.searchPoke}
+          placeholder='Search Pokemon'
+          value={this.state.Pokesearch}
+          onChangeText={(pokeSearch)=>this.setState({pokeSearch})}
+        />
+        <Image style={styles.img} source={myBackground}/>
         </View>
 
 
       );
     }
+}
+
+const styles = {
+  img: {
+    height: 400,
+    width: 400,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 }
 
 export default Search;
